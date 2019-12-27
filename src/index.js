cb80eec1740e0a79434731e03a9a4d9cf00c68df
Diego Go <diego.gomes87@gmail.com>
2019-12-27 14:08:19 -0300

    started logic js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('hello');
+const todos = ( title, description, dueData, priority, id, category) =>{ 
+  const getTitle = () => title;
+  const getDescription =()  => description
+  const getdueDate = () => dueData
+  const getPriority = () => priority
+  const getId = () => id
+  const getCategory = () => category
+
+  return {getTitle, getDescription, getdueDate, getPriority, getId, getCategory}
+}
+
+
