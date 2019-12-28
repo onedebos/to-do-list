32f55bbeb685f6b0bd93405d764e4a15e08c371e
Adebola <onedebos@gmail.com>
2019-12-28 13:21:33 +0100

    render categories to DOM

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('hello');
+/* eslint-disable react/react-in-jsx-scope */
+const categoryContainer = document.querySelector('[data-categories]');
+let categories = ['name', 'todo'];
+
+const render = () =>{
+    categories.forEach(category => {
+        const liElement = document.createElement('li');
+        liElement.classList.add('category-item');
+        liElement.innerHTML  = `<i class="tiny material-icons">crop_square</i> ${category}`
+        categoryContainer.appendChild(liElement);
+       
+
+
+    });
+    // clearElement(categoryContainer);
+}
+
+
+console.log("Opotoyi")
+
+render();
