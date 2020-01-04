266d230e172e9c39703923a99c0f354a266aa05a
Adebola <onedebos@gmail.com>
2020-01-04 18:49:33 +0100

    refactor collapsing todos

--- src/views/toDoViews.js
+++ src/views/toDoViews.js
@@ -1,11 +1,8 @@
 import sharedViews from "../views/sharedViews";
 
-
 // eslint-disable-next-line no-undef
 const index = require("../index");
 import formatDistanceToNow from "/home/adebola/Documents/microverse/javascript/to-do-list/node_modules/date-fns/formatDistanceToNow";
-
-
 
 const ToDoView = (() => {
   const renderToDo = selectedCat => {
@@ -14,34 +11,32 @@ const ToDoView = (() => {
       const createToDoLi = document.createElement("li");
       createToDoLi.dataset.todoId = todo.id;
       createToDoLi.innerHTML = `
-              
+      <div class="collapsible-header">
+      
+      <i class="material-icons">attach_file</i>${todo.title}
+      <div class="col s6">
+      <span>Due in ${formatDistanceToNow(
+        new Date(todo.dueDate),
+        true
+      )}</span>
+      </div> 
+      </div>
+      <div class="collapsible-body white-text">
+      <span>${todo.description}</span>
+  <p>Due date: ${todo.dueDate}</p>
+  <p>${todo.priority}</p>
+  <div class="del-hover">
+      <i class="material-icons del-todo" data-del-todo-btn=${todo.id}>delete</i>
+      </div>
+      <div class="del-hover">
+      <i class="material-icons del-todo" data-del-edit-btn=${todo.id}>create</i>
+      </div> 
+      </div>
         
-            <div class="collapsible-header">
-              <div class="col">
-                <i class="material-icons">attach_file</i>
-                
-              </div>
-              <strong>${todo.title}</strong>
-              <div class="col s6"><span>Due in ${formatDistanceToNow(
-                new Date(todo.dueDate),
-                true
-              )}</span></div>
-              <div class="col del-hover">
-              <i class="material-icons del-todo" data-del-todo-btn=${todo.id}>delete</i>
-              
-            </div>
-              <div
-            </div>
-            <div class="collapsible-body white-text">
-              <span>${todo.description}</span>
-              <p>Due date: ${todo.dueDate}</p>
-              <p>${todo.priority}</p>
-            </div>
+            
         `;
 
       index.getToDoUl.appendChild(createToDoLi);
-     
-
     });
   };
 
